28d40072264d337e6168369cb465d4cdaf07bd2a
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2022-06-25 13:44:56 -0700

    Fixed carousel feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  banners:any[] = [{
+    id:1,
+    img:'../../assets/banner/CityLights_cover.png',
+    alt:'miki_美稀_liveinfo_citylights_banner'
+  },
+  {
+    id:2,
+    img:'../../assets/banner/おことばに甘えて.png',
+    alt:'miki_美稀_liveinfo_onemanlove_banner'
+  },
+  {
+    id:3,
+    img:'../../assets/banner/ぷらそにか.png',
+    alt:'miki_美稀_plusonicainfo_ぷらそにか_banner'
+  },  
+  {
+    id:4,
+    img:'../../assets/banner/美稀.png',
+    alt:'miki_美稀_banner'
+  }]
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.banners[0].img);
   }
 
 }
